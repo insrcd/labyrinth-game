1bdb8312e2f4c80606587d50096f47aa5fda55c6
Adam Shaw <adam.shaw@foresite.com>
2020-08-16 18:07:03 -0400

    move up stationary

--- src/player.rs
+++ src/player.rs
@@ -1,18 +1,34 @@
 
+
+
+
+use bevy::{ prelude::* , prelude::Properties};
+
+    
+use std::fmt::{Formatter, Result};
+
+
 use crate::world::Location;
 
 struct Position(u32, u32);
 
-#[derive(PartialEq, Debug)]
+#[derive(PartialEq, Debug, Default)]
 pub struct Moving(pub Location, pub Location, pub Direction);
 
 #[derive(Clone, Copy, PartialEq, Debug)]
 pub enum Direction {
+    Stationary,
     Up,
     Down,
     Left,
-    Right,
-    Stationary
+    Right
+}
+
+impl std::fmt::Display for Player {
+    fn fmt(&self, f: &mut Formatter) -> Result {
+        // Customize so only `x` and `y` are denoted.
+        write!(f, "A player!")
+    }
 }
 
 pub enum Job {
