2c6a113fcb242d1cae99a580743df539ad3a8cb6
Adam Shaw <adam.shaw@slalom.com>
2020-08-24 16:53:47 -0400

    Builder is now retaining titles

--- crates/lab-builder/src/systems.rs
+++ crates/lab-builder/src/systems.rs
@@ -41,6 +41,23 @@ pub fn update_tile_system (mut commands : Commands,
         *t.0.y_mut() = mouse.position.y();
     }
 }
+pub fn select_tile_system (mut commands : Commands, 
+    mouse : ResMut<Mouse>,
+    windows: Res<Windows>,
+    mouse_input: Res<Input<MouseButton>>,
+    mut camera_query: Query<(&Camera, &Translation)>,
+    mut tile_query: Query<(&TileType, &SpriteInfo, &mut Translation, &Scale, &Draw)>){
+    
+    if mouse_input.just_pressed(MouseButton::Left) {
+        for (tt, si, t, scale, d) in &mut tile_query.iter() {
+            let true_location = get_true_location(&mouse, scale, &windows, &mut camera_query);
+            if  true_location.x() > t.0.x() * scale.0 && true_location.x() < t.0.x() + si.size().y() * scale.0 
+                 && true_location.y() > t.0.y() * scale.0 && true_location.x() < (t.0.y() + si.size().y()) * scale.0  {
+                    println!("Click on sprite {}", si.name);
+            }
+        }
+    }
+}
 pub fn add_tiles_to_world_system (
     mut commands: Commands,
     selected_tile: Res<SelectedTile>, 
@@ -73,21 +90,38 @@ pub fn add_tiles_to_world_system (
         let sprite: SpriteInfo = clone.sprite.clone();
 
         clone.location = Location(x, y, st.level,  world::WorldLocation::World);
-        
+                
         commands
             .spawn(sprite.to_components( Vec3::new(x,y,st.level), Scale(scroll_state.current_scale)))
             .with_bundle(clone);      
-        
+         
+            // no idea why I have to desapwn and then spawn again, for some reason this removes the freetile sprite
+            // but the entity remains, might be a bevy bug, might be a screw up on my part.
+            // TODO Investigate this further.Abilities
         for (e, n,t) in &mut tile_query.iter(){
             commands.despawn(e);
         }
-        
         commands                          
             .spawn(sprite.to_components(Vec3::new(mouse.position.x(), mouse.position.y(), 100.), Scale(scroll_state.current_scale)))
                 .with(FreeTile);      
     }
 }
+/// Helper function to get the true location of a mouse click 
+pub fn get_true_location(mouse : &ResMut<Mouse>, scale: &Scale, windows: &Res<Windows>, query: &mut Query<(&Camera,&Translation)>) -> Vec3 {
+    let window = windows.iter().last().unwrap();
+    
+    let mut camera_offset_x : f32 = 0.;
+    let mut camera_offset_y : f32 = 0.;
+    
+    for (c, t) in &mut query.iter(){
+        if *(c.name.as_ref()).unwrap_or(&"".to_string()) == "UiCamera" {
+            camera_offset_x = t.x();
+            camera_offset_y = t.y();
+        }
+    }
 
+    Vec3::new((mouse.position.x() + camera_offset_x) * scale.0, (mouse.position.y() + camera_offset_y) * scale.0, 0.)
+}
 pub struct FreeTile;
 
 
@@ -97,8 +131,7 @@ pub fn builder_keyboard_system (
     scroll : Res<ScrollState>,
     keyboard_input: Res<Input<KeyCode>>, 
     mut selected_tile: ResMut<SelectedTile>, 
-    mut palette: ResMut<TilePalette>, 
-    lib : Res<SpriteLibrary>,
+    palette: ResMut<TilePalette>, 
     mouse: Res<Mouse>,
     mut camera_query: Query<(&Camera, &Translation)>,
     mut free_tile: Query<(Entity, &FreeTile)>) {
@@ -146,7 +179,8 @@ pub fn builder_keyboard_system (
         let len = palette.tiles_in_category(&selected_tile.category).len();
         selected_tile.tile = (selected_tile.tile + 1) as usize % len;
 
-        let mouse_tile = palette.tiles_in_category(&selected_tile.category)[selected_tile.tile as usize];
+        let mouse_tile = palette.tiles_in_category(&selected_tile.category)[selected_tile.tile as usize];       
+
         for (n, _ft) in &mut free_tile.iter() {
             commands.despawn(n);
         }
