5c60c696c2b8556c91d0f9252eb85845cdc30190
Adam Shaw <adam.shaw@slalom.com>
2020-08-25 09:55:45 -0400

    some cleanup

--- crates/lab-world/src/systems.rs
+++ crates/lab-world/src/systems.rs
@@ -82,6 +82,7 @@ pub fn object_interaction_system (
     mut moveables: Query<Without<Player,(&Moveable, &mut Translation, Mutated<Movement>)>>,
     mut player_moved: Query<With<Player,(Entity, &mut Translation, Mutated<Movement>)>>
 ) {
+    // may split out CD and interaction at some point.    
 }
 /// Collision detection system
 /// 
@@ -91,45 +92,54 @@ pub fn tile_interaction_system (
     tile_pallette : ResMut<TilePalette>,   
     mut camera_query: Query<(&Camera, &mut Translation)>,
     mut wall_query: Query<(Entity, &mut TileType, &mut TileAttributes, &mut Translation, &crate::Interaction, &mut SpriteInfo)>,
-    mut moveables: Query<Without<Player,(&Moveable, &mut Translation, Mutated<Movement>, &SpriteInfo)>>,
+    mut moveables: Query<Without<Player,(&Moveable, &mut Translation, Mutated<Movement>, &SpriteInfo, &Scale)>>,
     mut player_moved: Query<With<Player,(Entity, &Scale, &mut Translation, Mutated<Movement>, &mut Inventory, &mut SpriteInfo)>>
-) {
-    let mut player_collision: Option<Translation> = None;
-    
-    // tile based collision
+) {    
     for (tile_entity, _tt,  
             tile_attributes, tile_translation, i,  
                 mut tile_sprite) in &mut wall_query.iter() {
 
-        for ( _m, mut move_transition, movement, move_sprite) in &mut moveables.iter() {
+        for ( _m, mut move_transition, movement, move_sprite,scale) in &mut moveables.iter() {
             println!("Checking for collision");
 
             let collision = collide(move_transition.0, 
-                move_sprite.size(), 
-                tile_translation.0, 
-                tile_sprite.size(), 
-                false);
+                (move_sprite.size() * scale.0) - Vec2::new(16.* scale.0,16.* scale.0),  tile_translation.0, tile_sprite.size() * scale.0, false);
             
             if let Some(collision) = collision {
                 match collision {
                     _ => { 
-                        if let InteractionResult::ChangeTile(attr) = (i.call)(InteractionContext {
+                        match (i.call)(InteractionContext {
                             interaction_location: Some(Location::from(*tile_translation)),
                             inventory: None,
                             player: None,
                             player_location: Some(movement.0.into()),
                             tile_attributes: Some(*tile_attributes),
                             tile_palette: Some(&*tile_pallette),
-                            sprite_info: Some(&move_sprite)
+                            sprite_info: Some(&*tile_sprite)
                         }) {      
-                            println!("Got change tile for NPC : {:?}", attr);                    
+                            InteractionResult::ChangeTile(attr) => {
+                           
+                            println!("Got change tile: {:?}", attr); 
                             
-                            commands.insert(tile_entity, (Location::from(*tile_translation), 
-                                attr, TextureAtlasSprite::new(tile_sprite.atlas_sprite)));
+                            //TODO: clean this up so a change to spriteinfo will change the tile
+                            tile_sprite.atlas_sprite = attr.sprite_idx.unwrap();
+
+                            commands.insert(tile_entity, (attr, Location::from(*tile_translation), 
+                                attr, TextureAtlasSprite::new(attr.sprite_idx.unwrap())));
+                            }
+
+                            InteractionResult::Damage(_) => {}
+                            InteractionResult::ChangeSprite(_) => {}
+                            InteractionResult::Move(_) => {}
+                            InteractionResult::PickUp(_) => {
+                                commands.despawn(tile_entity);
+                            },
+                            InteractionResult::Block => {
+                                *move_transition.0.x_mut() = (movement.0).0;
+                                *move_transition.0.y_mut() = (movement.0).1;
+                            }
+                            InteractionResult::None => {}
                         }
-                    
-                        *move_transition.0.x_mut() = (movement.0).0;
-                        *move_transition.0.y_mut() = (movement.0).1;
                     }
                 }
             } else {
@@ -137,8 +147,6 @@ pub fn tile_interaction_system (
             }
         } 
         for (e, scale, mut move_translation, movement, mut inventory, mut sprite) in &mut player_moved.iter() {
-            // I reduce the player bounding box by a few pixels to allow for closer interaction.
-            // this can probably be a non-constant based on tile size.
             let collision = collide(move_translation.0, 
                 (sprite.size() * scale.0) - Vec2::new(16.* scale.0,16.* scale.0),  tile_translation.0, tile_sprite.size() * scale.0, false);
             
@@ -241,7 +249,7 @@ pub fn zoom_system(
         } 
     }
 }
-/// Move all NPCs in the scene every 1.5 seconds
+/// Super Basic right now, Move all NPCs in the scene every 1.5 seconds
 pub fn npc_move_system(time: Res<Time>, mut query: Query<(&NonPlayer, &mut MoveTimer, &mut Translation, &mut Movement, &mut Moveable)>) {
     for (_npc, mut timer, mut trans, mut m, _mm) in &mut query.iter() {
         timer.0.tick(time.delta_seconds);
