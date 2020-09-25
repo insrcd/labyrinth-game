3b8b30878bb5695bd20b825058c3ecdab3f8fb1e
Adam Shaw <adam.shaw@slalom.com>
2020-09-25 13:03:49 -0400

    Add resources and tiles

--- resources/maps/Exterior.tsx
+++ resources/maps/Exterior.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.4" tiledversion="1.4.2" name="Exterier" tilewidth="32" tileheight="32" tilecount="384" columns="24">
- <image source="../Downloads/medieval tileset exterior.png" width="768" height="512"/>
+ <image source="../sprites/medieval tileset exterior.png" width="768" height="512"/>
 </tileset>
--- resources/maps/OutdoorTiles.tsx
+++ resources/maps/OutdoorTiles.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.4" tiledversion="1.4.2" name="OutdoorTiles" tilewidth="32" tileheight="32" tilecount="900" columns="30">
- <image source="../labrynth-game/resources/sprites/hyptosis_tile-art-batch-3.png" width="960" height="960"/>
+ <image source="../sprites/hyptosis_tile-art-batch-3.png" width="960" height="960"/>
 </tileset>
